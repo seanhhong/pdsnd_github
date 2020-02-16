694031a1cb288c67723b89b3e99282febae051d4
Sean <sean.hs.hong@gmail.com>
2020-02-15 19:34:07 -0800

    Bug fixed; removed raw data display by default

--- bikeshare.py
+++ bikeshare.py
@@ -195,7 +195,6 @@ def main():
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-        print(df.head(10))
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
